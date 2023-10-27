e41ecb54c01e171c32702dd0258522ffd51b5848
Cole0609 <cah062003@gmail.com>
2023-10-27 13:01:34 -0400

    added basic styling for entire document

--- project/src/App.js
+++ project/src/App.js
@@ -1,5 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "./styles.css";
 import { HashRouter } from "react-router-dom";
 import { Route, Routes, Navigate } from "react-router";
 import Profile from "./profile";
@@ -9,18 +9,17 @@ import signIn from "./signIn";
 import MessageBoard from "./Community";
 import Register from "./signIn/register";
 
-
 function App() {
 	return (
 		<HashRouter>
-			<div>
+			<div className="wd-main-page">
 				<Header />
 				<Routes>
-					<Route path="/" element={<Navigate to="Home"/>}></Route>
-          <Route path="Home" element={<Home />}></Route>
-          <Route path="Community" element={<MessageBoard />}></Route>
-          <Route path="signIn" element={<signIn />}></Route>
-          <Route path="register" element={<Register />}></Route>
+					<Route path="/" element={<Navigate to="Home" />}></Route>
+					<Route path="Home" element={<Home />}></Route>
+					<Route path="Community" element={<MessageBoard />}></Route>
+					<Route path="signIn" element={<signIn />}></Route>
+					<Route path="register" element={<Register />}></Route>
 					<Route path="Profile" element={<Profile />}></Route>
 				</Routes>
 			</div>
