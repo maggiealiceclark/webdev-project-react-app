6474fa2942295c6803295fec23e8cc7075b00f17
Cole0609 <cah062003@gmail.com>
2023-11-18 17:45:23 -0500

    added expand private message feature

--- project/src/Community/index.js
+++ project/src/Community/index.js
@@ -7,14 +7,13 @@ import ExpandedPrivateMessages from "./ExpandedPrivateMessages";
 function MessageBoard(props) {
 	const users = props.users;
 	const [expandedId, setExpandedId] = useState(0);
-
 	return (
 		<div>
 			<div className="wd-community-main">
 				<PrivateMessages setExpandedId={setExpandedId} />
 				<GlobalMessages />
 			</div>
-			{expandedId !== 0 && <ExpandedPrivateMessages expandedId={expandedId} users={users}/>}
+			{expandedId !== 0 && <ExpandedPrivateMessages expandedId={expandedId} users={users} />}
 		</div>
 	);
 }
