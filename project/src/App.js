d50a85aafb039affbd8b1f4748ca9a4f3e4ea4b9
Cole0609 <cah062003@gmail.com>
2023-11-18 16:51:36 -0500

    added user routes

--- project/src/App.js
+++ project/src/App.js
@@ -8,8 +8,25 @@ import Home from "./home";
 import signIn from "./signIn";
 import MessageBoard from "./Community";
 import Register from "./signIn/register";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 
 function App() {
+	const [users, setUsers] = useState([]);
+
+	const API_BASE = "http://localhost:4000/api";
+	const USERS_URL = `${API_BASE}/users`;
+
+	const fetchUsers = async () => {
+		const response = await axios.get(USERS_URL);
+		setUsers(response.data);
+	};
+
+	useEffect(() => {
+		fetchUsers();
+	}, []);
+
 	return (
 		<HashRouter>
 			<div className="wd-main-page">
