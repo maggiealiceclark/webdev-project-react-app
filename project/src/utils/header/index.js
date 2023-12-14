46aa3e5b8d661bf31f88d791abd889c5d360d3e2
Cole0609 <cah062003@gmail.com>
2023-12-14 12:14:33 -0500

    fixed user tables

--- project/src/utils/header/index.js
+++ project/src/utils/header/index.js
@@ -62,7 +62,6 @@ function Header({ setIsAuthenticated, onSignOut }) {
 				<Navbar.Toggle aria-controls="navbarScroll" />
 				<Navbar.Collapse id="navbarScroll">
 					<Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll>
-						<Nav.Link href="#/home">Home</Nav.Link>
 						<Nav.Link href="#/profile/">Profile</Nav.Link>
 						<Nav.Link href="#/community">Community</Nav.Link>
 						<Nav.Link href="#/search">Search</Nav.Link>
